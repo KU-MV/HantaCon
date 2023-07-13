7dac29f9ae9c6214d56ffb0fa5ff9aebe387cbdc
KU-MV <kmpark0131@korea.ac.kr>
2023-07-13 23:54:48 +0900

    test

--- preload.js
+++ preload.js
@@ -1,4 +1,8 @@
-const {ipcRenderer, contextBridge} = require('electron');
+const {ipcRenderer, contextBridge, shell} = require('electron');
+const base_path = process.env.INIT_CWD
+const ref_path = base_path + '/ref/'
+const result_path = base_path + '/result/'
+const log = require('electron-log')
 
 contextBridge.exposeInMainWorld('file_find', {
   openDialog() {
@@ -7,5 +11,11 @@ contextBridge.exposeInMainWorld('file_find', {
 
   runShell(items) {
     ipcRenderer.send('runShell', {value: items});
+  },
+
+  openBase() {
+    log.info("test")
+    log.info(base_path)
+    shell.openPath(base_path) 
   }
 });
