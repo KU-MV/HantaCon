7c576fe0408bfc646e2c236c92f400c97e353821
Grey Ha <greytrace64@gmail.com>
2023-08-06 11:44:24 +0900

    update

--- worker.js
+++ worker.js
@@ -38,16 +38,58 @@ parentPort.on('message', (items) => {
         let value = item["value"]
         let value_split = String(value).split('/')
         let value_split2 = String(value_split[value_split.length - 1]).split('.')
+        let result_name = value_split2[0]
         let option = item["option"]
         let option_name = option['name']
         let option_L = ref_path + option_name + '/' + option['L']
         let option_M = ref_path + option_name + '/' + option['M']
         let option_S = ref_path + option_name + '/' + option['S']
         let threshold = item['threshold']
-        let nextstrain_L = ''
-        let nextstrain_M = ''
-        let nextstrain_S = ''
-        
+        let nextstrain_L = data_path + '/nextstrain/htv_L/data/'
+        let nextstrain_M = data_path + '/nextstrain/htv_M/data/'
+        let nextstrain_S = data_path + '/nextstrain/htv_S/data/'
+        let sequence_L = nextstrain_L + 'sequence.fasta'
+        let metadata_L = nextstrain_L + 'metadata.tsv'
+        let sequence_M = nextstrain_M + 'sequence.fasta'
+        let metadata_M = nextstrain_M + 'metadata.tsv'
+        let sequence_S = nextstrain_S + 'sequence.fasta'
+        let metadata_S = nextstrain_S + 'metadata.tsv'
+
+        let virus = item['nextstrain']['virus']
+        let accession = item['nextstrain']['accession']
+        let date = item['nextstrain']['date']
+
+        let host = item['nextstrain']['host']
+        let region = item['nextstrain']['region']
+        let country = item['nextstrain']['country']
+
+        let city = item['nextstrain']['city']
+        let town = item['nextstrain']['town']
+        let db = item['nextstrain']['db']
+
+        let author = item['nextstrain']['author']
+        let url = item['nextstrain']['url']
+        let title = item['nextstrain']['title']
+
+        let journal = item['nextstrain']['journal']
+        let paper_url = item['nextstrain']['paper_url']
+
+        let metadata_text = virus + "\t"
+        metadata_text += accession + "\t"
+        metadata_text += date + "\t"
+        metadata_text += host + "\t"
+        metadata_text += region + "\t"
+        metadata_text += country + "\t"
+        metadata_text += city + "\t"
+        metadata_text += town + "\t"
+        metadata_text += db + "\t"
+        metadata_text += author + "\t"
+        metadata_text += url + "\t"
+        metadata_text += title + "\t"
+        metadata_text += journal + "\t"
+        metadata_text += paper_url + "\n"
+
+
         //micromamba run -n KU-ONT-Hantavirus-consensus nextflow
         //nextflow ./main.nf --fastq ./SEOV_80-39_10-5.fastq --prefix test --outdir test_output --L ./SEOV_Ref/SEOV_80-39_L.fasta --M ./SEOV_Ref/SEOV_80-39_M.fasta --S ./SEOV_Ref/SEOV_80-39_S.fasta
         log.info(value)
@@ -61,8 +103,8 @@ parentPort.on('message', (items) => {
           //'-c', '"'+data_path+'/nextflow.config"',
           'run', '"' + data_path + '/main.nf"',
           '--fastq', value,
-          '--prefix', value_split2[0],
-          '--outdir', result_path + value_split2[0],
+          '--prefix', result_name,
+          '--outdir', result_path + result_name,
           '--L', option_L,
           '--M', option_M,
           '--S', option_S,
@@ -79,16 +121,16 @@ parentPort.on('message', (items) => {
           //log.info(result.stdout.toString())
           log.info('완료: ', item)
         }
-        let bams_L = result_path + value_split2[0] + "/bams/" + value_split2[0] + "_L.bam"
-        let bams_M = result_path + value_split2[0] + "/bams/" + value_split2[0] + "_M.bam"
-        let bams_S = result_path + value_split2[0] + "/bams/" + value_split2[0] + "_S.bam"
+        let bams_L = result_path + result_name + "/bams/" + result_name + "_L.bam"
+        let bams_M = result_path + result_name + "/bams/" + result_name + "_M.bam"
+        let bams_S = result_path + result_name + "/bams/" + result_name + "_S.bam"
         let obj = {
           'item': item,
           'result': result,
           'app': app,
           'args': args,
           'bams': {
-            'name': value_split2[0],
+            'name': result_name,
             'L': bams_L,
             'M': bams_M,
             'S': bams_S
@@ -97,20 +139,23 @@ parentPort.on('message', (items) => {
          
         if (fs.existsSync(bams_L)) {
           bam_data = file_read(bams_L)
-          input_text = bam_data
-          file_write(file_path=nextstrain_L, content=input_text)
+          input_text = ">" + result_name + "\n" + bam_data + "\n"
+          file_write(file_path=sequence_L, content=input_text)
+          file_write(file_path=metadata_L, content=metadata_text)
         }
 
         if (fs.existsSync(bams_M)) {
           bam_data = file_read(bams_M)
-          input_text = bam_data
-          file_write(file_path=nextstrain_M, content=input_text)
+          input_text = ">" + result_name + "\n" + bam_data + "\n"
+          file_write(file_path=sequence_M, content=input_text)
+          file_write(file_path=metadata_M, content=metadata_text)
         }
 
         if (fs.existsSync(bams_S)) {
           bam_data = file_read(bams_S)
-          input_text = bam_data
-          file_write(file_path=nextstrain_S, content=input_text)
+          input_text = ">" + result_name + "\n" + bam_data + "\n"
+          file_write(file_path=sequence_S, content=input_text)
+          file_write(file_path=metadata_S, content=metadata_text)
         }
 
         parentPort.postMessage(obj); 
